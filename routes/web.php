4d65a977e7cd08480ae88dff0a75396e657f68ca
blacksheep212 <palerj183@gmail.com>
2025-07-08 15:48:08 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -6,67 +6,67 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('admin')->group(function () {
     // Dashboard - Make this the default admin route
     Route::get('/', function () {
-        return view('livewire.admin');
+        return view('admin.admin');
     })->name('admin.dashboard');
 
 
     // Add routes
     Route::get('/add', function () {
-        return view('livewire.add.add');
+        return view('add.add');
     })->name('admin.add');
 
     Route::get('/add/books', function () {
-        return view('livewire.add.book');
+        return view('add.book');
     })->name('admin.add.books');
 
     Route::get('/add/electronic', function () {
-        return view('livewire.add.electronic');
+        return view('add.electronic');
     })->name('admin.add.electronic');
 
     Route::get('/add/periodical', function () {
-        return view('livewire.add.periodical');
+        return view('add.periodical');
     })->name('admin.add.periodical');
 
     Route::get('/add/thesis', function () {
-        return view('livewire.add.thesis');
+        return view('add.thesis');
     })->name('admin.add.thesis');
 
     Route::get('/add/user', function () {
-        return view('livewire.add.user');
+        return view('add.user');
     })->name('admin.add.user');
 
     Route::get('/add/school', function () {
-        return view('livewire.add.school');
+        return view('add.school');
     })->name('admin.add.school');
 
     // Update routes
     Route::get('/update', function () {
-        return view('livewire.update.update');
+        return view('update.update');
     })->name('admin.update');
 
     Route::get('/update/user', function () {
-        return view('livewire.update.user');
+        return view('update.user');
     })->name('admin.update.user');
 
     Route::get('/update/school-info', function () {
-        return view('livewire.update.school');
+        return view('update.school');
     })->name('admin.update.school-info');
 
     // Accounts routes
     Route::get('/accounts', function () {
-        return view('livewire.accounts.account');
+        return view('accounts.account');
     })->name('admin.accounts');
 
     Route::get('/accounts/manage', function () {
-        return view('livewire.accounts.manage');
+        return view('accounts.manage');
     })->name('admin.accounts.manage');
 
     // Other routes
     Route::get('/account-settings', function () {
-        return view('livewire.admin.settings'); // Updated to a more specific view
+        return view('admin.settings'); // Updated to a more specific view
     })->name('admin.account-settings');
 
     Route::get('/history', function () {
-        return view('livewire.admin.history'); // Updated to a more specific view
+        return view('admin.history'); // Updated to a more specific view
     })->name('admin.history');
 });
